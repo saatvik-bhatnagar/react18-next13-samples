d0d9ac4be9d45487443d89e81e2cea14f8298719
Saatvik Bhatnagar <s-bhatnagar@mercari.com>
2022-12-12 21:23:15 +0900

    Added sample for parallel data fetching with Suspense

--- app/page.tsx
+++ app/page.tsx
@@ -1,57 +1,51 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import React, { Suspense } from "react";
+import { JsxElement } from "typescript";
+const getPokemonData = async () => {
+  const res = await fetch(`https://pokeapi.co/api/v2/pokemon/?limit=20`);
+  return new Promise((resolve) => {
+    setTimeout(resolve, 5000);
+  }).then(() => res.json());
+};
+const getAbilitiesData = async () => {
+  const res = await fetch(`https://pokeapi.co/api/v2/ability/?limit=10`);
 
-export default function Home() {
+  return new Promise((resolve) => {
+    setTimeout(resolve, 2000);
+  }).then(() => res.json());
+};
+
+export default async function Page() {
+  const pokemonData = getPokemonData();
+  const pokemonAbilities = getAbilitiesData();
   return (
-    <div className={styles.container}>
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js 13!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://beta.nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js 13</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Explore the Next.js 13 playground.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates/next.js/app-directory?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>Deploy your Next.js site to a public URL with Vercel.</p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
-    </div>
-  )
+    <>
+      <Suspense fallback={<div>Loading pokemons for 5 seconds...</div>}>
+        <Pokemon promise={pokemonData} />
+      </Suspense>
+      <Suspense fallback={<div>Loading abilities for 2 seconds...</div>}>
+        <Ability promise={pokemonAbilities} />
+      </Suspense>
+    </>
+  );
 }
+
+const Pokemon = async ({ ...props }) => {
+  const result = await props.promise;
+  return (
+    <ul>
+      {result.results.map((pokemon: any) => (
+        <li key={pokemon.name}>{pokemon.name}</li>
+      ))}
+    </ul>
+  );
+};
+const Ability = async ({ ...props }) => {
+  const result = await props.promise;
+  return (
+    <ul>
+      {result.results.map((ability: any) => (
+        <li key={ability.name}>{ability.name}</li>
+      ))}
+    </ul>
+  );
+};
